104d9f184d3bd4028c1045864c6ab91382d6e977
Roazar <pasqualea.a@gmail.com>
2025-08-29 11:34:12 +0200

    risoluzione errore register

--- t-shirt-store/client/src/pages/Register.jsx
+++ t-shirt-store/client/src/pages/Register.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { getNames } from 'country-list';
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Register = () => {
   const [form, setForm] = useState({ 
