a6c9b6817d240035b6a0dc06cec28507eb851159
Roazar <pasqualea.a@gmail.com>
2025-09-27 15:49:43 +0200

    check nodemailer

--- t-shirt-store/server/server.js
+++ t-shirt-store/server/server.js
@@ -159,6 +159,14 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
